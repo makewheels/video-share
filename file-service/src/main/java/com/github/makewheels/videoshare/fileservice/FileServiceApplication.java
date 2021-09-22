b9ed604ef251f6a2fa897df9f01800db36227002
makewheels <makewheels>
2021-09-22 10:55:22 +0800

    新增文件微服务

---
+++ file-service/src/main/java/com/github/makewheels/videoshare/fileservice/FileServiceApplication.java
@@ -0,0 +1,17 @@
+package com.github.makewheels.videoshare.fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class FileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileServiceApplication.class, args);
+    }
+
+}
