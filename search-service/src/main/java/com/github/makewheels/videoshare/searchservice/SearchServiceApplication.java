54ebf74b914467df2ea6df1a7460e825c1cef060
makewheels <makewheels>
2021-10-03 21:48:34 +0800

    新增搜索微服务

--- search-service/src/main/java/com/github/makewheels/videoshare/searchservice/SearchServiceApplication.java
+++ search-service/src/main/java/com/github/makewheels/videoshare/searchservice/SearchServiceApplication.java
@@ -2,7 +2,16 @@ package com.github.makewheels.videoshare.searchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
+@EnableFeignClients
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {
+        "com.github.makewheels.videoshare",
+        "com.github.makewheels.universaluserservice"
+})
 @SpringBootApplication
 public class SearchServiceApplication {
 
