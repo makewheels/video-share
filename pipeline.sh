67c1103b1098ba9c7beb10c73970d8d96c2fde2e
makewheels <makewheels>
2021-10-04 18:05:54 +0800

    pipeline.sh

---
+++ pipeline.sh
@@ -0,0 +1,16 @@
+tar zxvf /home/admin/${PIPELINE_NAME}/package.tgz -C /home/admin/${PIPELINE_NAME}/
+
+chmod a+x /home/admin/${PIPELINE_NAME}/video-service/deploy.sh
+sh /home/admin/${PIPELINE_NAME}/video-service/deploy.sh restart
+
+chmod a+x /home/admin/${PIPELINE_NAME}/file-service/deploy.sh
+sh /home/admin/${PIPELINE_NAME}/file-service/deploy.sh restart
+
+chmod a+x /home/admin/${PIPELINE_NAME}/transcode-service/deploy.sh
+sh /home/admin/${PIPELINE_NAME}/transcode-service/deploy.sh restart
+
+chmod a+x /home/admin/${PIPELINE_NAME}/search-service/deploy.sh
+sh /home/admin/${PIPELINE_NAME}/search-service/deploy.sh restart
+
+chmod a+x /home/admin/${PIPELINE_NAME}/api-gateway/deploy.sh
+sh /home/admin/${PIPELINE_NAME}/api-gateway/deploy.sh restart
