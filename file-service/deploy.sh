019d25e316c93c478558e50adf42f0e9db0b93c4
makewheels <makewheels>
2021-10-04 18:54:14 +0800

    pipeline.sh

--- file-service/deploy.sh
+++ file-service/deploy.sh
@@ -9,7 +9,7 @@ APP_START_TIMEOUT=20                                                  # 等待�
 APP_PORT=5016                                                         # 应用端口
 HEALTH_CHECK_URL=http://java8.icu:${APP_PORT}/${APP_NAME}/healthCheck # 应用健康检查URL
 APP_HOME=/home/admin/${APP_NAME}                                      # 从package.tgz中解压出来的jar包放到这个目录下
-JAR_NAME=${APP_HOME}/file-service/target/${APP_NAME}-1.0.0.jar                     # jar包的名字
+JAR_NAME=${APP_HOME}/file-service/target/file-service-1.0.0.jar                     # jar包的名字
 JAVA_OUT=${APP_HOME}/file-service/logs/start.log                                   #应用的启动日志
 
 # 创建出相关目录
